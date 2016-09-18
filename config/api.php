91a91ace0a2089e5599a57ba20995e91e76b227f
tyb917 <461670819@qq.com>
2016-09-18 18:41:53 +0800

    oauth认证

--- config/api.php
+++ config/api.php
@@ -172,7 +172,17 @@ return [
             return new Dingo\Api\Auth\Provider\Basic($app['auth']);
         },
         'oauth' => function ($app) {
-            return new Dingo\Api\Auth\Provider\OAuth2($app['oauth2-server.authorizer']->getChecker());
+            $provider = new Dingo\Api\Auth\Provider\OAuth2($app['oauth2-server.authorizer']->getChecker());
+
+            $provider->setUserResolver(function ($id) {
+                // Logic to return a user by their ID.
+            });
+
+            $provider->setClientResolver(function ($id) {
+                // Logic to return a client by their ID.
+            });
+
+            return $provider;
         }
     ],
 
